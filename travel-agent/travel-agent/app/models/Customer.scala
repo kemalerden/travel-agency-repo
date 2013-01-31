d1f40da81f5699838a82d6031a12084820bfe927
Yavuz <yavuz.aksu@yahoo.com>
2013-01-31 03:43:36 +0000

    new methods

--- travel-agent/travel-agent/app/models/Customer.scala
+++ travel-agent/travel-agent/app/models/Customer.scala
@@ -1,4 +1,13 @@
 package models
 
-case class Customer(val name:String , val surname:String){
+case class Customer(val name:String , val surname:String)
+
+object Customer {
+  
+  def all(): List[Customer] = Nil
+  
+  def create(name: String, surname:String) {}
+  
+  def delete(id: Long) {}
+  
 }
