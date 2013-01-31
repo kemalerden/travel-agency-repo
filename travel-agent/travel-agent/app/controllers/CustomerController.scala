a83686d75e631200eb09d1e4f34e3130a4787176
Yavuz <yavuz.aksu@yahoo.com>
2013-01-31 06:20:45 +0000

    Customer Registration Form

--- travel-agent/travel-agent/app/controllers/CustomerController.scala
+++ travel-agent/travel-agent/app/controllers/CustomerController.scala
@@ -2,14 +2,23 @@ package controllers
 
 import play.api._
 import play.api.mvc._
+import play.api.data._
+import play.api.data.Forms._
+
 
 object CustomerController extends Controller {
   
   def customer = Action {
-    Ok(views.html.customer("Customers"))
+    Ok(views.html.customer(customerForm))
   }
  
 
+	val customerForm = Form(
+	   tuple(
+        "name" -> nonEmptyText,
+	    "surname" -> nonEmptyText
+	  )
+	)
 
 
   def getCustomers = TODO
